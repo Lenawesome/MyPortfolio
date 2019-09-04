056225294ed03d0147e4e4091c29c977e012737e
Lenawesome <nawilldominate@gmail.com>
2019-09-04 21:42:51 +0700

    Add content to home page (no hope)

--- js/scroll.js
+++ js/scroll.js
@@ -2,9 +2,11 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 64 || document.documentElement.scrollTop > 64) {
-    document.getElementById("menu-wrapper").style.padding = "40px 0px";
+    document.getElementById("menu-wrapper").style.padding = "20px 0px";
+    document.getElementById("menu-wrapper").style.background = "black";
   } else {
      
-    document.getElementById("menu-wrapper").style.padding = "20px 0px";
+    document.getElementById("menu-wrapper").style.padding = "40px 0px";
+    document.getElementById("menu-wrapper").style.background = "transparent";
   }
 }
