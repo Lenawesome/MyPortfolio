5efb8714a8022813a66bc22883cf45f428e11816
Lenawesome <nawilldominate@gmail.com>
2019-09-05 20:55:59 +0700

    Add reponsive content

--- js/active.js
+++ js/active.js
@@ -1,27 +1,27 @@
-var menuItems = document.getElementsByClassName("menumobile__item");
-        for (var i = 0; i < menuItems.length; i++) {
-            menuItems[i].addEventListener("click", function () {
-                var current = document.getElementsByClassName("menumobile__item__active");
-                current[0].className = current[0].className.replace(" menumobile__item__active", "");
-                this.className += " menumobile__item__active";
-            })
-        }
-        var menuItems = document.getElementsByClassName("sidebar__item");
-        for (var i = 0; i < menuItems.length; i++) {
-            menuItems[i].addEventListener("click", function () {
-                var current = document.getElementsByClassName("sidebar__item__active");
-                current[0].className = current[0].className.replace(" sidebar__item__active", "");
-                this.className += " sidebar__item__active";
-            })
-        }
-        var menuItems = document.getElementsByClassName("menu-link");
-        for (var i = 0; i < menuItems.length; i++) {
-            menuItems[i].addEventListener("click", function () {
-                var current = document.getElementsByClassName("active");
-                current[0].className = current[0].className.replace(" active", "");
-                this.className += " active";
-            })
-        }  
+// var menuItems = document.getElementsByClassName("menumobile__item");
+//         for (var i = 0; i < menuItems.length; i++) {
+//             menuItems[i].addEventListener("click", function () {
+//                 var current = document.getElementsByClassName("menumobile__item__active");
+//                 current[0].className = current[0].className.replace(" menumobile__item__active", "");
+//                 this.className += " menumobile__item__active";
+//             })
+//         }
+//         var menuItems = document.getElementsByClassName("sidebar__item");
+//         for (var i = 0; i < menuItems.length; i++) {
+//             menuItems[i].addEventListener("click", function () {
+//                 var current = document.getElementsByClassName("sidebar__item__active");
+//                 current[0].className = current[0].className.replace(" sidebar__item__active", "");
+//                 this.className += " sidebar__item__active";
+//             })
+//         }
+//         var menuItems = document.getElementsByClassName("menu-link");
+//         for (var i = 0; i < menuItems.length; i++) {
+//             menuItems[i].addEventListener("click", function () {
+//                 var current = document.getElementsByClassName("active");
+//                 current[0].className = current[0].className.replace(" active", "");
+//                 this.className += " active";
+//             })
+//         }  
 function expandSidebar() {
             var contentElement = document.getElementById("content");
             var sidebarElement = document.getElementById("sidebar");
